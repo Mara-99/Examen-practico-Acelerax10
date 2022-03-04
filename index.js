8298cbd2b6121bb0b5d854afe7e84a435ae35759
Mara Pérez <90355396+Mara-99@users.noreply.github.com>
2022-03-03 23:35:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("hello")
+
+
+// EJERCICIO 1
+function generar() {
+    let numRan = Math.floor(Math.random()*100);
+    
+    if(numRan%2==0) {
+        console.log(numRan+" // el numero es par");
+    }else{
+        console.log(numRan+" // el numero es impar")
+    }
+}
+
+generar()
+generar()
+generar()
+generar()
+generar()
+
+
+
+
+
+
+
